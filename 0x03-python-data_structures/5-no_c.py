8b1ed1dc9507547c38a2bdbfcf2cf7df9d799bce
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-07 23:59:22 +0200

    Mandatory Python 0x03 | A string Without C | QuickFix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ""
     for i in my_string:
-        if ord(i) != 97 and ord(i) != 67:
+        if ord(i) != 99 and ord(i) != 67:
             new_string += i
     else:
         return new_string
