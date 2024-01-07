0a5e2a691773f870d61951036405f27b4f957d67
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-07 23:29:14 +0200

    Mandatory Python 0x03 | Element at nth index | Fix V2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(mylist) - 1):
+    if idx < 0 or idx >= len(mylist):
         return None
-    for indx, element in enumerate(my_list):
-        if indx is idx:
-            return my_list[indx]
+    return my_list[idx]
