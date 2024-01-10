0917eb721ca31ffde1dbaa90cd96b152bf421183
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-11 00:56:13 +0200

    Mandatory Python 0x04 | Sorted Dictionnary | Small Fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for k, i in a_dictionary.items():
+    for k, i in sorted(a_dictionary.items()):
         print("{}: {}".format(k, i))
