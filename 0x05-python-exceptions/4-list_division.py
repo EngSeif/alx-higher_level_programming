588c0daeaeabd155aa0fc78269a902722e5f7edb
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-22 16:58:46 +0200

    Mandatory Python 0x05 | Divide a list | Fix Error V2

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             Q = 0
         except ZeroDivisionError:
             print("division by 0")
+            Q = 0
         finally:
             NewList.append(Q)
     return NewList
