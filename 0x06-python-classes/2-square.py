54181f521841767ae4d002c66d891a66e5a0869a
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-23 21:08:48 +0200

    Mandatory 0x06 Python | Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This Module Defines Class: Square With Size
+    And Validates If the Size Entered is Valid
+"""
+
+
+class Square:
+    """ Class of Square That Intializes
+    the Size of Square 
+    """
+    __size = 0
+
+    def __init__(self, size = 0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
