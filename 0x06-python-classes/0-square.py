bb7ce292ec6ca3a2ea830fba885f5c527d18375d
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-23 20:56:55 +0200

    Mandatory 0x06 Python | Square Empty and Def Square | CodeBeauty edit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Square:
+    """ An Empty Class of Square That does Nothing"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
+    """ Class of Square That Intializes
+    the Size of Square
+    """
     __size = 0
 
     def __init__(self, size):
