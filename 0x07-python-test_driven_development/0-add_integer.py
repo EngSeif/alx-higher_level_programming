e70c842f7dd0e5e3ad48b265ea930ebc8fc46dbd
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-27 17:21:54 +0200

    Mandatory Python 0x07 | Add_int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This Module is For :
+- <add_integer> Function
+
+Author : Seif Mohamed
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Description: This Function Adds Two intergers
+    Arguments :
+        - a : First Integer
+        - b : Second Integer
+    Return : a + b if the Checks are CompleteW
+    """
+
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    else:
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        return a + b
