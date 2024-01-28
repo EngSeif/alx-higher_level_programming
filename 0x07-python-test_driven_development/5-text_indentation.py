e06a4e6377f6b6df028c49ff18c7f20db3d48a0f
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-28 15:45:10 +0200

    Mandatory Python 0x07 | Text Indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This Module is For :
+- <text_indentation> Function
+
+Author : Seif Mohamed
+"""
+
+def text_indentation(text):
+
+    """
+    Description:    prints a text with 2 new lines after
+                    each of these characters: . ? and :
+    Arguments :
+        - Text : Text To perform Function On
+    Return : None
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    delimiters = ".?:"
+    current = ""
+    for Element in text:
+        if Element in delimiters:
+            print((current + Element).strip())
+            print("\n", end="")
+            current = ""
+        else:
+            current += Element
+    print(current.strip(), end="")
