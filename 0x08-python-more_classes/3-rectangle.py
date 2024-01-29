abf14e1b44a25fca640df4b671f7d3513160f4ba
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-29 18:05:00 +0200

    Mandatory Python 0x08 | Print __str__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" This Module Defines an Empty Class Rectangle """
+
+
+class Rectangle:
+    """ An Empty Class of Rectangle That does Nothing"""
+
+    def __init__(self, width=0, height=0):
+        """ Constructor Of Class Instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+
+        """ Getter : Returns The Width Of Rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        """ Setter : Sets The Width Of Rectangle """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+
+        """ Getter : Returns The Height Of Rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter : Sets The Height Of Rectangle """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Returns The Area Of Rectangle """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ Returns The Perimeter Of Rectangle """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """ Prints Rectangle With # on Console"""
+        result = ""
+        if self.width == 0 or self.height == 0:
+            return result
+        else:
+            for i in range(0, self.height):
+                result += "#" * self.width + "\n"
+        return result.strip()
