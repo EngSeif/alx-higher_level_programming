abd8faf24e2a4cbfd3e9127b58321576882b5589
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-29 17:52:55 +0200

    Mandatory Python 0x08 | Add Area And Perimeter Methods | Fix Error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         """ Returns The Perimeter Of Rectangle """
         if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.width * self.height)
+        return 2 * (self.width + self.height)
