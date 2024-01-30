345a24f6344361e8bfd7d0a636ce2d27a610ad94
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-30 20:16:14 +0200

    Advanced Python 0x09 | Low Cost | New Sol

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,9 +4,4 @@
 
 class LockedClass:
     """ Class That Has No Attributes """
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            super().__setattr__(name, value)
-        else:
-            raise AttributeError("'{}' object has no attribute '{}'"
-                                 .format("LockedClass", name))
+    __slots__ = ["first_name"]
