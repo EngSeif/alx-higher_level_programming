245df213d26ef93a2d5e616f4f684c3af9ab8142
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-30 20:09:12 +0200

    Advanced Python 0x09 | Low Cost | Documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" This Module Contains Locked Class """
+
+
 class LockedClass:
+    """ Class That Has No Attributes """
     def __setattr__(self, name, value):
         if name == "first_name":
             super().__setattr__(name, value)
