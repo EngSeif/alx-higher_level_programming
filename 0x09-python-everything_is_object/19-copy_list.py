d4108b933536574c11ecd49a75e5c8978e8485f1
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-30 18:36:53 +0200

    Mandatory Python 0x09 | Answer Pt 5

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def copy_list(l):
     return l[:]
+
+my_list = [1, 2, ["Hamada", "Gamal"]]
+print(my_list)
+
+new_list = copy_list(my_list)
+
+print(my_list)
+print(new_list)
+
+print(new_list == my_list)
+print(new_list is my_list)
