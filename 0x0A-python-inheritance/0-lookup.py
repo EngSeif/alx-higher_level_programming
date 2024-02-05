5eb685b32da3adf7542aff6ba43392a1f09a9b48
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-05 20:55:20 +0200

    Mandatory Python 0x0A | LookUp Func | CodeBeauty Fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+This Module Has:
+-   lookup(obj) Function
+"""
+
+
 def lookup(obj):
+    """
+    This Function :
+        returns the list of available attributes
+        and methods of an object
+    """
     return dir(obj)
