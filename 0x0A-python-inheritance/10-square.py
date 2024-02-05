7a99bfdf225ec2cfdccb69844c83dfd5649ba4a7
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 00:26:44 +0200

    Mandatory Python 0x0A | Fix 10 and 11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     """ Class Square """
     def __init__(self, size):
         self.integer_validator("size", size)
-        super.__init__(size, size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
