5ed31befc523bc93da5bb7562fa34ed508c37167
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-05 22:53:12 +0200

    Mandatory Python 0x0A | Raise Area error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This Module Has:
+-   BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    Inheritance : None
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
