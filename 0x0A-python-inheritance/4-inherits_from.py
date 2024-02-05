3d18a699e14e00a44562c8453b3a5a12b554c754
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-05 22:47:46 +0200

    Mandatory Python 0x0A | Is SubClass? | Fix Errors

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
         a class that inherited (directly or indirectly)
         from the specified class ; otherwise False.
     """
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
