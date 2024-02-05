90128a4cf344346d155fa218c55f110da722b5a3
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 00:14:04 +0200

    Mandatory Python 0x0A | Square 10 and 11 | CodeBeauty

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,6 +9,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Class Square """
     def __init__(self, size):
         self.integer_validator(size)
         self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,11 +9,12 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Class Square """
     def __init__(self, size):
         self.integer_validator(size)
         self.__size = size
 
-    def area(size):
+    def area(self):
         return self.__size ** 2
 
     def __str__(self):
