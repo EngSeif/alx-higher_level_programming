4b06685863611ac9fc9fef421e7aa29e99016b21
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 16:34:32 +0200

    Mandatory Python 0x0B | Append To File

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This Module Contains :-
+    -    write_file(filename="", text="")
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Description:
+    -   Write Text To File And
+        Returns Num Of Chars In Text
+
+    Args:
+    -   filename : Name Of The File (str)
+    -   text : Text To Be Added In File
+
+    Return :
+        None
+    """
+    with open(filename, mode="a", encoding="utf-8") as rFile:
+        return rFile.write(text)
