932f9c35f0ca2c00844e09e4609c0c4ed1cf0416
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 15:58:35 +0200

    Mandatory Python 0x0B | Read File

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This Module Contains :-
+    -    read_file(filename="") Function
+"""
+
+
+def read_file(filename=""):
+    """
+    Description:
+    -   Reads Content Of File And Print It
+
+    Args:
+    -   filename = Name Of The File (str)
+
+    Return :
+        None
+    """
+    with open(filename, encoding="utf-8") as rFile:
+        print(rFile.read())
