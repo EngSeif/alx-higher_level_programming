5e9da7864f93cc79e666cb10acfdd22e5060ba02
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 16:45:11 +0200

    Mandatory Python 0x0B | JSON : Save Object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This Module Contains :-
+    -    save_to_json_file(my_obj, filename)
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Description:
+    -  writes an Object to a text file, using a JSON representation
+
+    Args:
+    -   filename : Name Of The File (str)
+    -   my_obj : Object To Convert Into JSON Representation
+
+    Return :
+        None
+    """
+    with open(filename, mode="a", encoding="utf-8") as rFile:
+        json.dump(my_obj, rFile)
