f015a97a7f0a1641944d29f449477529cd86f095
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-06 17:06:52 +0200

    Mandatory Python 0x0B |  Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This Script :
+    adds all arguments to a Python list, and then save them to a file
+"""
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    try:
+        existing_items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        existing_items = []
+    existing_items.extend(args)
+    save_to_json_file(existing_items, "add_item.json")
