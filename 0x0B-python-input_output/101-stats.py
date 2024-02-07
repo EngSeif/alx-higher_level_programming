65d3a7babc138769f9a951644db587d167a6de01
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-07 16:59:11 +0200

    Advanced Python 0x0B | Log parsing | Test 19

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,14 +11,15 @@ import sys
 def Print_Data(t_size, status_dict):
     print("File size: {}".format(t_size))
     for code in sorted(status_dict.keys()):
+        if status_dict[code] > 0:
             print("{}: {}".format(code, status_dict[code]))
 
 def main():
     total_size = 0
     line_count = 0
-    Status_Dict = {'200': 0, '301': 0, '400': 0,
-                    '401': 0, '403': 0, '404': 0,
-                    '405': 0, '500': 0}
+    Status_Dict = dict()
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+
     try:
         for line in sys.stdin:
             line_count += 1
@@ -31,9 +32,11 @@ def main():
             sp_line = line.split()
             try:
                 total_size += int(sp_line[-1])
-                status_code = sp_line[-2]
-                if status_code in Status_Dict.keys():
-                    Status_Dict[status_code] += 1
+                if line[-2] in valid_codes:
+                    if Status_Dict.get(line[-2], -1) == -1:
+                        Status_Dict[line[-2]] = 1
+                    else:
+                        Status_Dict[line[-2]] += 1
             except (ValueError, IndexError):
                 pass
         Print_Data(total_size, Status_Dict)
