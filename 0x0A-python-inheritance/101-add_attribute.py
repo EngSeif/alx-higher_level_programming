8483fe7f018b740f58949c29d3479acc1e0af494
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-07 17:40:18 +0200

    Advanced Python 0x0A | Can I ? | fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,10 @@ This Module Has:
 
 
 def add_attribute(obj, attr, value):
+    """
+    This Function :
+        adds a new attribute to an object if it’s possible
+    """
     if hasattr(obj, "__dict__"):
         setattr(obj, attr, value)
     else:
