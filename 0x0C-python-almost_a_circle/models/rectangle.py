62bdc164a93e3e5c695efdeacee98c323f842afa
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-10 20:47:42 +0200

    Mandatory Python 0x0C | Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,15 @@ class Rectangle(Base):
         for h in range(0, self.height):
             print(" " * self.x + "#" * self.width)
 
+    def update(self, *args):
+        if len(args) > 0:
+            if len(args) >= 1:
+                super().__init__(args[0])
+            self.width = args[1] if len(args) >= 2 else self.width
+            self.height = args[2] if len(args) >= 3 else self.height
+            self.x = args[3] if len(args) >= 4 else self.x
+            self.y = args[4] if len(args) >= 5 else self.y
+
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
