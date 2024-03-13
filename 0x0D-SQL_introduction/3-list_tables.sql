26a3f515f43fc1918b725df7db88a5ec7d32dfe2
EngSeif <eng.seifmohamed2004@gmail.com>
2024-03-13 22:46:25 +0200

    Mandatory 0x0D SQL | First Table

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,2 @@
--- a script that deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn’t exist, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
+-- a script that lists all the tables of a database in your MySQL server
 SHOW TABLES;
