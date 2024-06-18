f26ab94f74b5682e078cef3e560966ee99343669
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-18 23:19:42 +0300

    0x12 JavaScript WarmUp | Add func | fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 const num1 = parseInt(args[0], 10);
-const num2 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
 
 function add (a, b) {
   return (a + b);
