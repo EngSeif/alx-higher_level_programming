c3f1ca1fcfa63ce6ed7453a2e0d7524fb35b9382
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-21 19:58:34 +0300

    0x0F Py ORM | FILTER by User input | format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name\
-                = %s \
-                ORDER BY states.id ASC", (argv[4], ))
+    com = "SELECT * FROM states WHERE name = '{:s}'\
+        ORDER BY states.id ASC".format(argv[4])
+    cur.execute(com)
     Result = cur.fetchall()
     for row in Result:
         print(row)
