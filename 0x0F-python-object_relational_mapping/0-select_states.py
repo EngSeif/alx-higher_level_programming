1118784a58519780791b4221afd4b8cb34d90c11
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-21 18:47:09 +0300

    0x0F Py ORM | SELECT | Fix style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" List States """
+
 import MySQLdb
 from sys import argv
-db = MySQLdb.connect(
-    host = "localhost",
-    port = 3306,
-    user = argv[1],
-    passwd = argv[2],
-    db = argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-Result = cur.fetchall()
-for row in Result:
-    print(row)
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    Result = cur.fetchall()
+    for row in Result:
+        print(row)
