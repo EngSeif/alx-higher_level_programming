369bded4eb7db6b69eb00d85f450ad6ef9642492
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-22 18:33:28 +0300

    0x0F Py ORM | State Module

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" State Module """
+
+from sqlalchemy import Column, INTEGER, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        State Class
+    """
+    __tablename__ = "state"
+    id = Column(
+                "id", INTEGER,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True
+                )
+    name = Column(
+        "name",
+        VARCHAR(128),
+        nullable=False
+        )
