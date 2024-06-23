4d9ede31c86419cc8dda133571af272f7cad4812
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-23 13:08:45 +0300

    0x0F Py ORM | Fix 2 | Handle Error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,20 +5,23 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    cur = db.cursor()
-    com = """
-            SELECT * FROM states
-            WHERE name = '{}'
-            ORDER BY states.id ASC;""".format(argv[4])
-    cur.execute(com)
-    Result = cur.fetchall()
-    for row in Result:
-        print(row)
-    cur.close()
-    db.close()
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+        cur = db.cursor()
+        com = """
+                SELECT * FROM states
+                WHERE name = '{}'
+                ORDER BY states.id ASC;""".format(argv[4])
+        cur.execute(com)
+        Result = cur.fetchall()
+        for row in Result:
+            print(row)
+        cur.close()
+        db.close()
+    except Exception as e:
+        exit()
