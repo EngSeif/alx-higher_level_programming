a6d0133777486dc2915845f39d355f0dd2e095ab
EngSeif <eng.seifmohamed2004@gmail.com>
2024-06-23 13:12:36 +0300

    0x0F Py ORM | Fix 2 | Try Fetchone

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
                 WHERE name = '{}'
                 ORDER BY states.id ASC;""".format(argv[4])
         cur.execute(com)
-        Result = cur.fetchall()
-        for row in Result:
-            print(row)
+        Result = cur.fetchone()
+        print(Result)
         cur.close()
         db.close()
     except Exception as e:
