764c01ddfe51dd127c84154858e2ce4bfb854e69
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-04 21:06:38 +0300

    0x10 Network 0 | Test Complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,14 +3,25 @@
 Module of find_peak Function
 """
 
+def getBigger(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
 def find_peak(list_of_integers):
     """
     Find Biggest Number
     """
-    if len(list_of_integers) == 0:
+    big = 0
+    size = len(list_of_integers) - 1
+    if  len(list_of_integers) == 0:
         return None
-    big = -9000
-    for num in list_of_integers:
-        if num > big:
-            big = num
+    half = (size // 2) + 1
+    for i in range(0, half):
+        if i == 0:
+            big = getBigger(list_of_integers[i], list_of_integers[size-i])
+        else:
+            if big < getBigger(list_of_integers[i], list_of_integers[size-i]):
+                big = getBigger(list_of_integers[i], list_of_integers[size-i])
     return big
