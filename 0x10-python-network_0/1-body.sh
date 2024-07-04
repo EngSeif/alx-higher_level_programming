b93594c91f9eeefddc62deb1ebbe19bece77c107
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-04 19:20:31 +0300

    0x10 Network 0 | Body Request Fix 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-S=$(curl -so /dev/null -w '%{http_code}\n' "$1"); if [ "$S" -eq 200 ]; then curl -s "$1"; fi
+S=$(curl -so /dev/null -w '%{http_code}' "$1"); if [ "$S" -eq 200 ]; then curl -s "$1"; fi
