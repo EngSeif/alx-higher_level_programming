4239794133e13c7238ab5364cc271c8f74ad9374
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-04 21:50:02 +0300

    0x10 Network 0 | finish Code

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,26 +3,22 @@
 Module of find_peak Function
 """
 
-def getBigger(num1, num2):
-    if num1 > num2:
-        return num1
-    else:
-        return num2
-
 def find_peak(list_of_integers):
     """
     Find Biggest Number
     """
-    if  len(list_of_integers) == 0:
+    # Base cases
+    if len(list_of_integers) == 0:
         return None
-    midpoint = len(list_of_integers) // 2
-    half_1 = list_of_integers[:midpoint]
-    half_2 = list_of_integers[midpoint:]
-    if sum(half_1) > sum(half_2):
-        if len(half_1) == 1:
-            return half_1[0]
-        return find_peak(half_1)
-    else:
-        if len(half_2) == 1:
-            return half_2[0]
-        return find_peak(half_2)
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    left = 0
+    right = len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]
