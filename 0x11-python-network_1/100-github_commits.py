9a7b91456ba7307e09fb5b06b5b0f7069861da98
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-05 21:17:16 +0300

    0x10 Network 0 | GIthub API Commets

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    username = argv[1]
+    Repo_name = argv[2]
+    url = f"https://api.github.com/repos/{username}/{Repo_name}/commits"
+    r = requests.get(url)
+
+    for coms in r.json():
+        sha = coms['sha']
+        name = coms['author']['login']
+        print(f"{sha}: {name}")
