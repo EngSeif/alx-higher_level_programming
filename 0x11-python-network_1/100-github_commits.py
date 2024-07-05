f0b894106560caed38a62d83bf248b7892fb6943
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-05 21:21:17 +0300

    0x10 Network 0 | GIthub API Commets | fix V2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
 
     for coms in r.json():
         sha = coms['sha']
-        name = coms['commit']['committer']['name']
+        name = coms['commit']['author']['name']
         print(f"{sha}: {name}")
