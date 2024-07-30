ef603daafd3bf0438e69268645e574ad35d03103
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-30 20:27:06 +0300

    JS Web Scrap | Who is Playing in Right Order | fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,25 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request-promise');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-const options = {
-  url,
-  json: true
-};
 
-const op = {
-  url: '',
-  json: true
-};
-
-request(options, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    for (let i = 0; i < body.characters.length; i++) {
-      op.url = body.characters[i];
-      request(op, (err, r, b) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(b.name);
-        }
+async function fetchData (url) {
+  try {
+    const filmResponse = await request({
+      url,
+      json: true
+    });
+    const charUrl = filmResponse.characters;
+    for (let i = 0; i < charUrl.length; i++) {
+      const characterResponse = await request({
+        uri: charUrl[i],
+        json: true
       });
+      console.log(characterResponse.name);
     }
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-});
+}
+
+fetchData(url);
