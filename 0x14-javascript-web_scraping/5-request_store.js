aac2669941b8bffc1dc074ea35b5ac05d159858a
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-30 17:04:09 +0300

    JS Web Scrap | Loripusm

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(filePath, body, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
